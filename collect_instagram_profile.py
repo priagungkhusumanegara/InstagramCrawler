0dd118cb238b0fe527dc3bc0a99160d9ef3cf006
Priagung Khusumanegara <priagung.123@gmail.com>
2018-08-03 10:13:52 +0700

    Add files via upload

---
+++ collect_instagram_profile.py
@@ -0,0 +1,42 @@
+__author__ = "Priagung Khusumanegara"
+__copyright__ = "Copyright 2018, Instagram Crawler"
+__credits__ = ["Instabot"]
+__license__ = "GPL"
+__version__ = "1.0.0"
+__maintainer__ = "Prigung Khusumanegara"
+__email__ = "priagung.123@gmail.com"
+__status__ = "Development"
+
+# lib
+import os
+import sys
+import pandas as pd
+from pandas.io.json import json_normalize
+from instabot import Bot
+
+# Your Username and Password 
+bot = Bot()
+bot.login(username= "<username>" , password="<password>")
+
+# dataframe
+data = pd.DataFrame([])
+
+# list of target name
+with open('list_of_name.txt') as f:
+    content = f.readlines()
+
+
+content = [x.strip() for x in content] 
+
+for val in content:
+    bot.search_users(val)
+    jsondata = bot.last_json
+    X = json_normalize(jsondata['users'])
+    if len(X) > 0:
+        Y = X.assign(Name=val)
+        Z = Y.iloc[0]
+        data = data.append(Z)
+    else:
+        pass
+
+print(data)
